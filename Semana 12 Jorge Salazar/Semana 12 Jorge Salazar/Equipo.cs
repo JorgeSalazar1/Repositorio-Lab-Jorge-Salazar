4f7f0fbf1542a8c99db0ec23780f5e902316c755
JorgeSalazar1 <jorgesalazarcerin2014@gmail.com>
2022-11-10 09:16:06 -0600

    Semana 12

---
+++ Semana 12 Jorge Salazar/Semana 12 Jorge Salazar/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana_12_Jorge_Salazar
+{
+    internal class Equipo
+    {
+        public string nombre;
+
+    }
+}
